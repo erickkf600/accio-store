4c0a6f359239849bc0f112cb593ecba2bf21f048
erickkf600 <erickkf600@gmail.com>
2018-12-12 19:21:04 -0200

    somente offline

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,9 +13,9 @@ export class HomePage {
   products: any[];
   constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
     this.WooCommerce = WC({
-      url:"http://accio-estoque.epizy.com",
-      consumerKey: "ck_476bbb2a6bb9eb85afdfd8ced49bdc58fd086817",
-      consumerSecret: "cs_a9a4ac980716241180c439b937d2be4aec8aa461",
+      url:"http://localhost/ACCIO-STORE/",
+      consumerKey: "ck_c44ae4f28cad1f59b33e0a1648a19e1431ddd0ae",
+      consumerSecret: "cs_4703ff399dbb21c75460cbbea33b4a50f0aee464",
       wpAPI: true,
       version: "wc/v2",
       queryStringAuth: true
